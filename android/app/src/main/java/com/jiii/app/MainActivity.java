ab8af554fdbe2a509eeaa41b1410817b0f97539d
Albert0803 <ton-email@exemple.com>
2026-01-12 10:50:11 +0300

    Passage final a Java 21 et reparation config

---
+++ android/app/src/main/java/com/jiii/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jiii.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jiii.app',
+  appName: 'JIII',
+  webDir: 'dist'
+};
+
+export default config;
